3545684390be7b5fa123264fd8a3f524c053dd48
Jean Nunes <jaon@itriad.org.br>
2020-11-05 03:42:11 -0400

    validate outcome transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,6 @@
-// import AppError from '../errors/AppError';
-
 import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
 import Transaction from '../models/Transaction';
 import Category from '../models/Category';
 
@@ -20,8 +20,15 @@ class CreateTransactionService {
     type,
     category,
   }: Request): Promise<Transaction> {
-    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
     const categoryRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+      if (value > balance.total) {
+        throw new AppError('Valor acima do limite permitido.', 400);
+      }
+    }
 
     let categoryDatabase = await categoryRepository.findOne({
       where: {
@@ -36,14 +43,14 @@ class CreateTransactionService {
       await categoryRepository.save(categoryDatabase);
     }
 
-    const transaction = transactionRepository.create({
+    const transaction = transactionsRepository.create({
       category_id: categoryDatabase.id,
       title,
       type,
       value,
     });
 
-    await transactionRepository.save(transaction);
+    await transactionsRepository.save(transaction);
 
     return transaction;
   }

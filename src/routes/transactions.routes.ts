16e69eecbf4d1ea20369b6d9bd7cd5b482f9958a
Jean Nunes <jaon@itriad.org.br>
2020-11-10 12:39:36 -0400

    Relations mapping

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -13,10 +13,7 @@ const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
-  const transactions = await transactionsRepository.find({
-    relations: ['category'],
-    select: ['id', 'title', 'value', 'type', 'created_at', 'updated_at'],
-  });
+  const transactions = await transactionsRepository.find();
   const balance = await transactionsRepository.getBalance();
 
   return response.json({ transactions, balance });
